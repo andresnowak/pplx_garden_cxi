2a00018f4160c98036e8c794f203bdce29d56a1a
Andres Nowak <andresnowak52@gmail.com>
2026-05-04 15:57:18 +0200

    Added other debug dicts

--- python-ext/src/py_p2p_all_to_all.rs
+++ python-ext/src/py_p2p_all_to_all.rs
@@ -226,6 +226,15 @@ impl PyAllToAllContext {
         dict.set_item("source_rank", state.source_rank)?;
         dict.set_item("padded_index", state.padded_index)?;
         dict.set_item("num_recv_tokens", state.num_recv_tokens)?;
+        dict.set_item("sum_tokens_per_expert", state.sum_tokens_per_expert)?;
+        dict.set_item("num_recv_tokens_main", state.num_recv_tokens_main)?;
+        dict.set_item("num_recv_efa_tokens", state.num_recv_efa_tokens)?;
+        dict.set_item("total_padded_tokens", state.total_padded_tokens)?;
+        dict.set_item("max_padded_index", state.max_padded_index)?;
+        dict.set_item(
+            "padded_index_out_of_bounds",
+            state.padded_index_out_of_bounds,
+        )?;
         Ok(dict)
     }
 }
