64fd0f51bbd8d7d4aa0ef5fab3b8992aeae771b2
Andres Nowak <andresnowak52@gmail.com>
2026-05-12 14:07:22 +0200

    Fix: Added a kill gpu processes before running the tests
    This is needed because when a test fails there can be sometimes left over processes as the destruction after the failure of tests didn't work correctly, and if there are left over processes it seems the tests will start to fail randomly.

--- test_all2all.sh
+++ test_all2all.sh
@@ -8,6 +8,14 @@ export RUST_BACKTRACE=full
 export FI_CXI_ENABLE_WRITEDATA=1
 export PPLX_LOG_COLOR=never
 export DD_ENV=1
+export PPLX_TEST_DEBUG=1
+export PPLX_TEST_DEBUG_RECV_BUFFER=1
+export PPLX_TEST_DEBUG_COMBINE_WORKER=1
+export PPLX_TEST_DEBUG_COMBINE_RECV_KERNEL=1
+
+nvidia-smi --query-compute-apps=pid,process_name,used_memory --format=csv
+nvidia-smi --query-compute-apps=pid --format=csv,noheader | xargs -r kill # This is necessary because it seems if there are left over processes that didn't get killed when a test failed, then they will always cause subsequent tests to fail randomly
+nvidia-smi --query-compute-apps=pid,process_name,used_memory --format=csv
 
 # python3 -m pytest -sv -vv -rs tests/p2p_all_to_all/test_p2p_all_to_all.py::test_p2p_all_to_all "$@"
 
@@ -15,7 +23,7 @@ export DD_ENV=1
 # python3 -m pytest -sv -vv -rs "tests/p2p_all_to_all/test_p2p_all_to_all.py::test_p2p_all_to_all[TP4-DP2-NIC1-BF16-T1024]" "$@"
 # python3 -m pytest -sv -vv -rs "tests/p2p_all_to_all/test_p2p_all_to_all.py::test_p2p_all_to_all[TP4-DP2-NIC1-BF16-T1024]" "$@"
 # python3 -m pytest -sv -vv -rs "tests/p2p_all_to_all/test_p2p_all_to_all.py::test_p2p_all_to_all[TP4-DP2-NIC1-BF16]" "$@"
-python3 -m pytest -sv -vv -rs "tests/p2p_all_to_all/test_p2p_all_to_all.py::test_p2p_all_to_all[TP4-DP2-NVL2]" "$@"
+python3 -m pytest -sv -vv -rs "tests/p2p_all_to_all/test_p2p_all_to_all.py::test_p2p_all_to_all[TP4-DP2-NVL2-T256]" "$@"
 
 
 # python3 -m pytest -sv -vv -rs tests/p2p_all_to_all/test_p2p_all_to_all.py::test_p2p_all_to_all_moe_roundtrip "$@"
